88c30f7c02bc4b26dd1c2756be358dbdb3893189
Marián Trpkoš <majda.trpkos@gmail.com>
2022-03-28 09:28:17 +0200

    log perfomrance details

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ export default async function (options) {
                         tile.features.push(...mappedFeatures);
                     }
 
-                    if (options.log) {
+                    if (options.logPerformance) {
                         console.log(`Creating tile ${x} ${y} ${z}`);
                     }
 
@@ -195,7 +195,7 @@ export default async function (options) {
                                     'INSERT INTO tiles (zoom_level, tile_column, tile_row, tile_data) VALUES(?, ?, ?, ?)',
                                     z, x, zoomDimension - 1 - y, compressed));
 
-                            if (options.log) {
+                            if (options.logPerformance) {
                                 console.log(`Tile ${x} ${y} ${z} created`);
                             }
 
